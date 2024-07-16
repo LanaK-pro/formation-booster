be4a3ca2f3d5f8c4624aa43aa12f691d81b3b2cb
Lana Karmaoui <lana.karmaoui@gmail.com>
2024-07-16 16:53:50 +0200

    end of the day commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,8 +5,14 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  FIRST_ARTICLE = [
+    {
+      title: 'TITRE DU SITE',
+      content: 'test de content',
+      img: 'https://www.openlab-clermont.com/wp-content/uploads/2022/04/logo-human-booster.jpg',
+    },
+  ];
 }
